8d9ace7fe64766a0862e7305cb1582be5bad3c25
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2016-11-09 18:48:25 +0100

    Revert Build.php.

--- src/Snippets/Api/Build.php
+++ src/Snippets/Api/Build.php
@@ -16,8 +16,6 @@ class Build extends \Blogstep\AuthenticatedSnippet
         $structure = $this->m->files->get('site');
         $destination = $this->m->files->get('build');
         
-        $runner = new \Blogstep\Task\Runner('build');
-        
         $compiler = new \Blogstep\Build\Compiler($destination, $this->m->main->config['user']);
         $compiler->addTask(\Blogstep\Build\Task::load($this->m->main->p('src/tasks/copyStructure.php')));
         $compiler->addTask(\Blogstep\Build\Task::load($this->m->main->p('src/tasks/templateToPhp.php')));
@@ -25,46 +23,40 @@ class Build extends \Blogstep\AuthenticatedSnippet
         $compiler->addTask(\Blogstep\Build\Task::load($this->m->main->p('src/tasks/phpToText.php')));
         $compiler->addTask(\Blogstep\Build\Task::load($this->m->main->p('src/tasks/minifyAssets.php')));
         
-        $runner->add(
-            new \Blogstep\Task\UnitTask('clean', [$compiler, 'clean'], 'Emptying build directory'),
-            function () use($compiler, $content, $structure) {
-                $compiler->createContentTree($content);
-
-                $id = function ($content) { return $content; };
-                $compiler->content->addHandler('html', $id);
-                $compiler->content->addHandler('htm', $id);
-                $compiler->content->addHandler('md', [new \Parsedown(), 'text']);
-
-                $dir = scandir($this->m->main->p('src/filters'));
-                foreach ($dir as $file) {
-                    if (\Jivoo\Unicode::endsWith($file, '.php')) {
-                        $name = substr($file, 0, -4);
-                        $compiler->content->addFilter($name, require $this->m->main->p('src/filters/' . $file));
-                    }
-                }
-                if ($structure->get('filters')->getType() === 'directory') {
-                    foreach ($structure->get('filters') as $file) {
-                        if (\Jivoo\Unicode::endsWith($file->getName(), '.php')) {
-                            $name = substr($file->getName(), 0, -4);
-                            $compiler->content->addFilter($name, require $file->getRealPath());
-                        }
-                    }
-                }
-                $compiler->content->setDefaultFilters(['links']);
+        $compiler->clean();
+        $compiler->createContentTree($content);
+        
+        $id = function ($content) { return $content; };
+        $compiler->content->addHandler('html', $id);
+        $compiler->content->addHandler('htm', $id);
+        $compiler->content->addHandler('md', [new \Parsedown(), 'text']);
+        
+        $dir = scandir($this->m->main->p('src/filters'));
+        foreach ($dir as $file) {
+            if (\Jivoo\Unicode::endsWith($file, '.php')) {
+                $name = substr($file, 0, -4);
+                $compiler->content->addFilter($name, require $this->m->main->p('src/filters/' . $file));
             }
-        );
-        $runner->add(new \Blogstep\Task\UnitTask('structure', [$compiler, 'createStructure'], 'Creating initial site map'));
-        $runner->add($compiler);
-        $runner->add(new \Blogstep\Task\UnitTask('install', function () use ($compiler) {
-            $target = $this->m->main->config['user']->get('target', $this->m->main->p('target'));
-            $compiler->install($target);
-        }, 'Installing'));
+        }
+        if ($structure->get('filters')->getType() === 'directory') {
+            foreach ($structure->get('filters') as $file) {
+                if (\Jivoo\Unicode::endsWith($file->getName(), '.php')) {
+                    $name = substr($file->getName(), 0, -4);
+                    $compiler->content->addFilter($name, require $file->getRealPath());
+                }
+            }
+        }
         
-        $store = new \Jivoo\Store\SerializedStore($this->p('state/build.php'));
+        $compiler->content->setDefaultFilters(['links']);
         
-        $service = new \Blogstep\Task\Service($this->m->logger, $this->request, $store);
+        $compiler->createStructure($structure);
         
-        $service->run($runner);
+        $compiler->run();
+        
+        $target = $this->m->main->config['user']->get('target', $this->m->main->p('target'));
+        $compiler->install($target);
+        
+        return $this->ok();
     }
     
     public function get()
