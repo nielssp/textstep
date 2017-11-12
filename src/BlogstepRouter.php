1c25988652ce754961fee0a572c140d971ef57a9
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2017-11-12 15:52:33 +0100

    Fix buildQuery.

--- src/BlogstepRouter.php
+++ src/BlogstepRouter.php
@@ -10,7 +10,7 @@ namespace Blogstep;
  */
 class BlogstepRouter extends \Jivoo\Http\Router
 {
-    protected function buildQuery($query)
+    protected function buildQuery(array $query)
     {
         return str_replace('%2F', '/', parent::buildQuery($query));
     }
