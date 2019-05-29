4de3c3fcd017ba41ae49bd1ca4ab9d41270cb070
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2019-05-29 21:43:22 +0200

    Fix icon.

--- src/templates/workspace.html.php
+++ src/templates/workspace.html.php
@@ -5,7 +5,7 @@
 <title>TEXTSTEP</title>
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <?php echo $this->block('meta'); ?>
-<link rel="shortcut icon" href="<?php echo $this->link('asset:apps/workspace.app/icon.ico'); ?>" />
+<link rel="shortcut icon" href="<?php echo $this->link('asset:themes/default/icon.ico'); ?>" />
 <?php echo $this->resourceBlock("style"); ?>
 </head>
 <body data-server="<?php echo $this->link('path:api'); ?>" data-run="<?php echo $run; ?>" data-args="<?php echo $args; ?>">
